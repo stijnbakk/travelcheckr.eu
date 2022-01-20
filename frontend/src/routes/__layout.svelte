588bd8824edcfa3a3edbfb587a1b20428ff44839
Stijn Bakker <stijn@stijnbakker.com>
2022-01-20 17:33:07 +0100

    bugfix

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -13,8 +13,8 @@
 
 	
 
-	<div class="w-full bottom-0 fixed p-3">
-		<div class="w-full bg-yellow-300 text-black font-bold p-3 rounded text-left text-sm text-center">
+	<div class="w-full bottom-0 p-3">
+		<div class="w-full bg-yellow-300 text-black font-bold p-3 rounded text-sm text-center">
 			<div>
 				Welcome! Please note this website is not yet officially launched. Some things might not work as expected<br/><span class='font-normal'>Congratulations! You're now an official beta user 😎. <a class="underline underline-offset-2" href="/feedback">I'd love to know who you are and how you think I improve the site</a></span>
 			</div>
