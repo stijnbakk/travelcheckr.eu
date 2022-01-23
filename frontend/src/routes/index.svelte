1e0f6dae3c21b1720164ee518b14925df20479dd
Stijn Bakker <stijn@stijnbakker.com>
2022-01-23 21:18:46 +0100

    add og tags

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -8,12 +8,23 @@ import Feedback from '$lib/components/landingPage/Feedback.svelte';
 </script>
 
 
-
+<!-- <meta property="og:title" content="Check travel rules and COVID restrictions - TravelCheckr.eu">
+<meta property="og:site_name" content="TravelCheckr.eu">
+<meta property="og:url" content="https://www.travelcheckr.eu">
+<meta property="og:description" content="Quickly make sense of the maze of COVID rules. Easily understand what you need to do, bring, follow and expect about your">
+<meta property="og:type" content="website">
+<meta property="og:image" content=""> -->
 
 <svelte:head>
 	<title>Check travel rules and COVID restrictions - TravelCheckr.eu</title>
 	<meta name="robots" content="noindex nofollow" />
 	<html lang="en" />
+	<meta property="og:title" content="Check travel rules and COVID restrictions - TravelCheckr.eu">
+	<meta property="og:site_name" content="TravelCheckr.eu">
+	<meta property="og:url" content="https://www.travelcheckr.eu">
+	<meta property="og:description" content="Quickly make sense of the maze of COVID rules. Easily understand what you need to do, bring, follow and expect about your">
+	<meta property="og:type" content="website">
+	<!-- <meta property="og:image" content=""> -->
 </svelte:head>
 
 
