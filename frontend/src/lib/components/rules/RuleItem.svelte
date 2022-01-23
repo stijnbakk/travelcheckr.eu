92e5ca64f70f72e532606abb6c4b2964a4fab5be
Stijn Bakker <stijn@stijnbakker.com>
2022-01-23 15:31:22 +0100

    add url option

--- frontend/src/lib/components/rules/RuleItem.svelte
+++ frontend/src/lib/components/rules/RuleItem.svelte
@@ -1,10 +1,29 @@
 <script>
     export let ruleData;
+    console.log(ruleData)
 </script>
-<div class="flex mb-1  py-2 md:px-2 rounded md:ml-[-0.75rem] ">
-    <input type="checkbox" class="flex m-1 mr-3">
-    <div class="">
-        <p>{ruleData.title}</p>
-        <p class="text-xs">{ruleData.description}</p>
+
+
+{#if ruleData.link == null}
+
+    <div class="flex mb-1  py-2 md:px-2 rounded md:ml-[-0.75rem] ">
+        <input type="checkbox" class="flex m-1 mr-3">
+        <div class="">
+            <p>{ruleData.title}</p>
+            <p class="text-xs">{ruleData.description}</p>
+        </div>
     </div>
-</div>
+
+{:else}
+<a href="{ruleData.link}">
+    <div class="flex mb-1  py-2 md:px-2 rounded md:ml-[-0.75rem] ">
+        <input type="checkbox" class="flex m-1 mr-3">
+        <div class="">
+            <p>{ruleData.title}</p>
+            <p class="text-xs">{ruleData.description}</p>
+            <p class="text-blue-900 pt-2 pb-4 text-xs underline underline-offset-2">More information</p>
+        </div>
+    </div>
+</a>
+
+{/if}
