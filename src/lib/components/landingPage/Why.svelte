3378d5ea4348081ac14240692c00ebccd0770740
Stijn Bakker <stijn@stijnbakker.com>
2022-02-10 18:01:42 +0100

    fix typo

--- src/lib/components/landingPage/Why.svelte
+++ src/lib/components/landingPage/Why.svelte
@@ -3,6 +3,6 @@
         <p class="text-sm text-neutral-400 pb-4">Why build travelcheckr.eu?</p>
         <h1 class="font-bold font-serif text-4xl pb-4">COVID (travel) restrictions are a maze</h1>
         <p class="font-serif text-2xl pb-6">I try to help you make sense of all the rules.</p>
-        <p>I'm Stijn. Frustrated with chaotic rules I decided a little while ago to built this website. To help anyone going on holiday, or planning a trip. While I hope I can be of some value, this website is also a project for me to get back into programming. I hope you find this website useful. Fee free to browse around or <a href="mailto:stijn@travelcheckr.eu" class="underline underline-offset-2">reach out</a>.</p>
+        <p>I'm Stijn. Frustrated with chaotic rules I decided a little while ago to build this website. To help anyone going on holiday, or planning a trip. While I hope I can be of some value, this website is also a project for me to get back into programming. I hope you find this website useful. Fee free to browse around or <a href="mailto:stijn@travelcheckr.eu" class="underline underline-offset-2">reach out</a>.</p>
     </div>
 </div>
